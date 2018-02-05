3c0926fe10eee5e07e1af98d3e760af7ccd756c4
Rinno Master <luipi.alfaro@gmail.com>
2018-02-05 16:13:31 -0300

    Configurar allowed hosts heroku

--- CivicTest/settings.py
+++ CivicTest/settings.py
@@ -35,7 +35,9 @@ API_URL = 'https://maps.googleapis.com/maps/api/{}/json?'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'civictest.herokuapp.com'
+]
 
 
 # Application definition
