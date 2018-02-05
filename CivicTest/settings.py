558fbb7e910c93037e58438fc70363de34cb5c0c
Rinno Master <luipi.alfaro@gmail.com>
2018-02-05 16:49:44 -0300

    Permisos para rest framework

--- CivicTest/settings.py
+++ CivicTest/settings.py
@@ -65,7 +65,7 @@ MIDDLEWARE = [
 # Django rest_framework
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
